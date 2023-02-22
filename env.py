98ea3ba2457d1c5ff8cc3396cc5c3460199aa971
Xredys-19 <Meikelrizky145@gmail.com>
2023-02-22 11:33:36 +0700

    update

--- env.py
+++ env.py
@@ -1,7 +1,7 @@
 import openai
 #openai variable 
-openai.api_key = "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"
+openai.api_key = ""
 
 # telegram variable
-apiToken = '5419171626:AAE2mtB7jzJZYL2rp5L3_C-sC3hoX7KfVnw'
-chatID = '-791935333'
+apiToken = ''
+chatID = ''
